0f0a1cf41e232698056226443c19744bb3e076d0
MonoLogueChi <xxwhite.com@gmail.com>
2022-10-09 17:33:12 +0800

    更新重定向插件

--- docs/.vuepress/config/plugins/oldlink.ts
+++ docs/.vuepress/config/plugins/oldlink.ts
@@ -3,14 +3,21 @@ import { path } from "@vuepress/utils";
 import fs from "fs-extra";
 
 function htmlT(path: string): string {
-  return `<script>window.location.replace("${path}");</script>`;
+  return (
+    `<!DOCTYPE html><html><head><meta charset="utf-8">` +
+    `<link rel="canonical" href="${path}">` +
+    `<meta http-equiv="refresh" content="0; url='${path}'">` +
+    "</head>" +
+    `<body><script>window.location.replace("${path}")</script></body>` +
+    "</html>"
+  );
 }
 
 export default {
   name: "vuepress-plugin-oldlink",
   onGenerated: (app: App) => {
     app.pages.forEach((e) => {
-      if (e.frontmatter?.oldlink) {
+      if (e.frontmatter["oldlink"]) {
         let b = e.frontmatter.oldlink as Array<string>;
         b.forEach((f) => {
           let p = f.endsWith(".html")
