83c0effe60eb69b37540170dd00af24a0fd87905
MonoLogueChi <xxwhite.com@gmail.com>
2023-02-02 12:02:33 +0800

    添加静态压缩

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,13 +1,24 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig, viteBundler } from "vuepress";
+import viteCompression from "vite-plugin-compression";
 import theme from "./config/theme";
 import plugins from "./config/plugins";
 
 export default defineUserConfig({
   base: "/",
-  title:"帅比",
+  title: "帅比",
   dest: "dist",
   theme,
   shouldPrefetch: false,
   shouldPreload: false,
   plugins,
+  bundler: viteBundler({
+    viteOptions: {
+      plugins: [
+        viteCompression({
+          algorithm: "brotliCompress",
+          deleteOriginFile: false,
+        }),
+      ],
+    },
+  }),
 });
